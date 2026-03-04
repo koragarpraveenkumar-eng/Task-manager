adf57324bd8f71666912603a41e70e5092c4daf6
PRAVEENKUMAR KORAGAR <koragarpdk@gmail.com>
2026-03-04 15:03:04 +0530

    button update

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { ButtonContainer } from './style.js';
 
-const Button = ({ children, onClick, type, title, customProps }) => {
+const Button = ({ children, onClick, type, title, customProps, disabled=false }) => {
   return (
-    <ButtonContainer onClick={onClick} type={type} customProps={customProps}>
+    <ButtonContainer onClick={onClick} type={type} customProps={customProps}
+      disabled={disabled}
+    >
       {title || children}
     </ButtonContainer>
   );
