62a6fa5b3cc81eff225857c87ccced517dfccd81
PRAVEENKUMAR KORAGAR <koragarpdk@gmail.com>
2026-03-04 15:05:37 +0530

    added constant file

---
+++ src/constants/styleConstants.js
@@ -0,0 +1,12 @@
+export const buttonStyle = { 
+    height: '100%', 
+    minWidth:'100px',
+    borderRadius: '10px',
+    boxSizing: 'border-box',
+    cursor: 'pointer',
+    fontSize: '16px',
+    // fontWeight: 'bold',
+    backgroundColor: 'white',
+    border:'none',
+    padding:'0px'
+  }
