41742bb9405bdd330a55252604cda1a45acf29d4
PRAVEENKUMAR KORAGAR <koragarpdk@gmail.com>
2026-03-04 15:07:56 +0530

    processing task file created

--- src/pages/processingTask/index.js
+++ src/pages/processingTask/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { TaskContext } from '../../context/taskContext';
+import { ModalContext } from '../../context/modalContext';
+import TaskCard from '../../components/taskCard';
 
 function ProcessingTask() {
+  const { progressingTask } = useContext(TaskContext)
+
+  const {handleTaskToEdit} = useContext(ModalContext)
+
+  function onEdit(data){
+    handleTaskToEdit(data)
+  }
+
   return (
-    <h1>ProcessingTask</h1>
+     <div>
+      {
+       progressingTask?.length 
+       ?progressingTask?.map((value)=><TaskCard task={value} onEdit={onEdit}/>)
+       :<div>No Task</div>
+      }
+     </div>
   );
 }
 
