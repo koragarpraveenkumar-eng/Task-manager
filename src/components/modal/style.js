77d71d56f9fae1500126d5605f7b3716716469a6
PRAVEENKUMAR KORAGAR <koragarpdk@gmail.com>
2026-03-04 15:01:54 +0530

    modal style

--- src/components/modal/style.js
+++ src/components/modal/style.js
@@ -6,21 +6,19 @@ export const Box = styled('div')(({theme, customProps})=>({
     ...customProps
 }))
 export const ModalContainer = styled('div')(({theme, display})=>({
-    position:'absolute',
-    top:0,
+    position:'fixed',
+    top:'0',
     left:0,
     right:0,
     bottom:0,
+    zIndex:2,
     backgroundColor: 'rgba(0,0,0,0.5)',
     display:display,
-    // display:'flex',
-    // justifyContent:'center',
-    // alignItems:'center'
 }))
 
 export const ModalSubContainer = styled('div')(({theme})=>({
     width:'80%',
     // minHeight:'20%',
     backgroundColor:'white',
-    overflow:'auto'
+    overflow:'auto',
 }))
