470909d0f335a499a7beb7c77133050aea219fc1
PRAVEENKUMAR KORAGAR <koragarpdk@gmail.com>
2026-03-04 15:06:57 +0530

    completed tsk file created

--- src/pages/completedTask/index.js
+++ src/pages/completedTask/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { TaskContext } from '../../context/taskContext';
+import { ModalContext } from '../../context/modalContext';
+import TaskCard from '../../components/taskCard';
+
 
 function CompletedTask() {
+  const { completedTask } = useContext(TaskContext)
+
+  const {handleTaskToEdit} = useContext(ModalContext)
+
+  function onEdit(data){
+    handleTaskToEdit(data)
+  }
+
   return (
-    <h1>CompletedTask</h1>
+     <div>
+      {
+       completedTask?.length 
+       ?completedTask?.map((value)=><TaskCard task={value} onEdit={onEdit}/>)
+       :<div>No Task</div>
+      }
+     </div>
   );
 }
 
